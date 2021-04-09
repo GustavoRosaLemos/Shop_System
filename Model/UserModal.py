5cdc3c2ccc683ce9a6795a69e239630e91f197d1
Gustavo Lemos <dev.gustavolemos@outlook.com>
2021-04-09 09:12:15 -0300

    Update UserModal
    Finalizado a parte de objeto do usuário.

--- Model/UserModal.py
+++ Model/UserModal.py
@@ -2,12 +2,64 @@ import datetime
 
 
 class User:
-    def __init__(self, name, birth: datetime, cpf, email, pwd, active):
+    def __init__(self, name, birth, cpf, email, pwd, isAdmin):
         self.name = name
         self.birth = birth
         self.cpf = cpf
         self.email = email
         self.pwd = pwd
-        self.active = active(False)
+        self.isAdmin = isAdmin
+
+
+    def setname(self, name):
+        self.name = name
+
+
+    def setbirth(self, birth):
+        self.birth = birth
+
+
+    def setcpf(self, cpf):
+        self.cpf = cpf
+
+
+    def setemail(self, email):
+        self.email = email
+
+
+    def setpwd(self, pwd):
+        self.pwd = pwd
+
+
+    def setisadmin(self, isAdmin):
+        self.isAdmin = isAdmin
+
+
+    def getname(self):
+        return self.name
+
+
+    def getbirth(self):
+        return self.birth
+
+
+    def getcpf(self):
+        return self.cpf
+
+
+    def getemail(self):
+        return self.email
+
+
+    def getpwd(self):
+        return self.pwd
+
+
+    def getisAdmin(self):
+        return self.isAdmin
+
+
+
+
 
 
