b7622886b90d133deee662dfa239c57d6426b010
Gustavo Lemos <dev.gustavolemos@outlook.com>
2021-04-09 11:30:46 -0300

    Update UserController.py
    Finalizado o UserController

--- Controller/UserController.py
+++ Controller/UserController.py
@@ -7,43 +7,65 @@ class UserControl:
     # def __init__(self):
     #     super(UserController, self).__init__()
 
-    def add_user(self):
-        with open("../DatabaseController/Users.txt", "a") as file:
-            if self.id > 0:
-                file.write(str(self.id) + ";" + self.nome)
+    def add_user(self, user):
+        with open("Model/Users.txt", "r") as file:
+            file = file.read()
+            lista = literal_eval(file)
+        if len(lista) == 0:
+            id = 1
+        else:
+            id = str(int(lista[len(lista) - 1]["id"]) + 1)
+        lista.append({"id": id,
+                      "name": user.getname(),
+                      "birth": user.getbirth(),
+                      "cpf": user.getcpf(),
+                      "email": user.getemail(),
+                      "password": user.getpwd(),
+                      "admin": user.getadmin()
+                      })
+        with open("Model/Users.txt", "w") as file:
+            file.write(str(lista))
 
     def get_users(self):
-        users = []
-        with open("../DatabaseController/Users.txt", "r") as file:
-            for user_file in file:
-                user = tuple(user_file.strip().split(";"))
-                users.append(user)
-            file.read()
-        return file
-
-    def get_by_id(self):
-        with open("../Model/Users.txt", "r") as file:
+        with open("Model/Users.txt", "r") as file:
             file = file.read()
-            ids = ""
-            try:
-               ids = literal_eval(file)
-
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-            return ids["usersid"]
-
-    def update(self, id):
-        lista = []
-        with open("../DatabaseController/Users.txt", "r") as file:
-            file.read()
             try:
                 lista = literal_eval(file)
             except:
-                raise Exception("Não foi possível ler os arquios para atualizar.")
-        with open("../DatabaseController/Users.txt", "w") as file:
-            try:
-                file.write(lista)
-            except:
-                raise Exception("Não foi possível atualizar.")
+                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
+        return lista
 
-        print(lista)
+    def get_by_id(self, id):
+        with open("Model/Users.txt", "r") as file:
+            file = file.read()
+            try:
+               lista = literal_eval(file)
+            except:
+                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
+            for i in range(len(lista)):
+                if lista[i]["id"] == id:
+                    return lista[i]
+                    break
+            else:
+                return []
+
+    def update(self, user):
+        with open("Model/Users.txt", "r") as file:
+            file = file.read()
+            lista = literal_eval(file)
+        for i in range(len(lista)):
+            if lista[i]["id"] == user.getid():
+                if not user.getname() == lista[i]["name"]:
+                    lista[i]["name"] = user.getname()
+                if not user.getbirth() == lista[i]["birth"]:
+                    lista[i]["birth"] = user.getbirth()
+                if not user.getcpf() == lista[i]["cpf"]:
+                    lista[i]["cpf"] = user.getcpf()
+                if not user.getemail() == lista[i]["email"]:
+                    lista[i]["email"] = user.getemail()
+                if not user.getpwd() == lista[i]["password"]:
+                    lista[i]["password"] = user.getpwd()
+                if not user.getadmin() == lista[i]["admin"]:
+                    lista[i]["admin"] = user.getadmin()
+        with open("Model/Users.txt", "w") as file:
+            file.write(str(lista))
