fec7f46fb30017be5b8f81cf85307fde779798e9
Gustavo Lemos <dev.gustavolemos@outlook.com>
2021-04-09 09:24:21 -0300

    Update CategoryModal.py
    Finalizado a parte de objetos

--- Model/CategoryModal.py
+++ Model/CategoryModal.py
@@ -1,4 +1,16 @@
 class Category:
     def __init__(self, id, name):
-        self.name = name
         self.id = id
+        self.name = name
+
+    def setid(self, id):
+        self.id = id
+
+    def setname(self, name):
+        self.name = name
+
+    def getid(self):
+        return self.id
+
+    def getname(self):
+        return self.name
