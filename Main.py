ae04678eabd24037a44311093a9241777c77cc78
Gustavo Lemos <dev.gustavolemos@outlook.com>
2021-04-09 16:47:51 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,5 +4,3 @@ from Controller import HomeController
 DatabaseController.DatabaseController().initdatabase() #Inicia o banco de dados
 HomeController.main().initialize() #Inicia a parte visual
 
-
-
