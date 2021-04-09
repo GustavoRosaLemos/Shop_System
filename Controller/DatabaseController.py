f61be2e42202aebabe6ecc68842f2034a60b664b
Gustavo Lemos <dev.gustavolemos@outlook.com>
2021-04-09 15:08:41 -0300

    Update DatabaseController
    Agora quando executado o programa insere [] nos arquivos de texto automaticamente, o que é necessário para o .txt funcionar como um banco de dados.

--- Controller/DatabaseController.py
+++ Controller/DatabaseController.py
@@ -1,28 +1,28 @@
 class DatabaseController:
     def initdatabase(self):
-        # Criar a tabela ids caso não exista
-        try:
-            ids = open("Model/Ids.txt", "r")
-        except:
-            ids = open("Model/Ids.txt", "x")
-
         # Criar a tabela Users caso não exista
         try:
             users = open("Model/Users.txt", "r")
         except:
             users = open("Model/Users.txt", "x")
+            with open("Model/Users.txt", "w") as file:
+                file.write("[]")
 
         # Criar a tabela Products caso não exista
         try:
             products = open("Model/Products.txt", "r")
         except:
             products = open("Model/Products.txt", "x")
+            with open("Model/Products.txt", "w") as file:
+                file.write("[]")
 
         # Criar a tabela Category caso não exista
         try:
             categorys = open("Model/Category.txt", "r")
         except:
             categorys = open("Model/Category.txt", "x")
+            with open("Model/Category.txt", "w") as file:
+                file.write("[]")
 
         #print("Banco de dados iniciado!")
 
