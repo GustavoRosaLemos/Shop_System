5035b8dc84896f7624ac6751415c865ddbd8770b
Gustavo Lemos <dev.gustavolemos@outlook.com>
2021-04-09 12:54:51 -0300

    Update CategoryController.py
    Finalizado a parte de métodos do CategoryController

--- Controller/CategoryController.py
+++ Controller/CategoryController.py
@@ -1,17 +1,56 @@
-from Model import CategoryModal
-CategoryModal.Category()
+from ast import literal_eval
 
-class CategoryController(CategoryModal):
-    def __init__(self):
-        super(CategoryController, self).__init__()
 
-    def add_category(self):
-        pass
-    def get_categories(self):
-        pass
-    def get_by_id(self):
-        pass
-    def uptade(self):
-        pass
-    def delete(self):
-        pass
+class CategoryController():
+    def add_category(self, category, locale):
+        with open(locale, "r") as file:
+            file = file.read()
+            lista = literal_eval(file)
+        if len(lista) == 0:
+            id = 1
+        else:
+            id = str(int(lista[len(lista) - 1]["id"]) + 1)
+        lista.append({"id": id,
+                      "name": category.getname()
+                      })
+        with open(locale, "w") as file:
+            file.write(str(lista))
+    def get_categories(self, locale):
+        with open(locale, "r") as file:
+            file = file.read()
+            try:
+                lista = literal_eval(file)
+            except:
+                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
+        return lista
+    def get_by_id(self, id, locale):
+        with open(locale, "r") as file:
+            file = file.read()
+            try:
+                lista = literal_eval(file)
+            except:
+                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
+            for i in range(len(lista)):
+                if lista[i]["id"] == id:
+                    return lista[i]
+                    break
+            else:
+                return []
+    def update(self, category, locale):
+        with open(locale, "r") as file:
+            file = file.read()
+            lista = literal_eval(file)
+        for i in range(len(lista)):
+            if lista[i]["id"] == category.getid():
+                if not lista[i]["name"] == category.getname():
+                    lista[i]["name"] = category.getname()
+
+        with open(locale, "w") as file:
+            file.write(str(lista))
+    def delete(self, id, locale):
+        with open(locale, "r") as file:
+            file = file.read()
+            lista = literal_eval(file)
+        for i in range(len(lista)):
+            if lista[i]["id"] == id:
+                lista.pop(i)
