a6bbc4e73640343f38c9523c062de46504681ba7
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-04-10 19:41:57 -0300

    Update UserController
    Modificado para funcionar com objetos

--- Controller/UserController.py
+++ Controller/UserController.py
@@ -1,18 +1,22 @@
 from ast import literal_eval
 
+users = []
+with open("Model/Users.txt", "r") as file:
+    file = file.read()
+    try:
+        lista = literal_eval(file)
+    except:
+        raise Exception("Erro no banco de dados. Não foi possível converter ID.")
+users = lista
+
 class UserControl:
-    # def __init__(self):
-    #     super(UserController, self).__init__()
 
     def add_user(self, user, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            lista = literal_eval(file)
-        if len(lista) == 0:
+        if len(users) == 0:
             id = 1
         else:
-            id = str(int(lista[len(lista) - 1]["id"]) + 1)
-        lista.append({"id": id,
+            id = str(int(users[len(users) - 1]["id"]) + 1)
+        users.append({"id": id,
                       "name": user.getname(),
                       "birth": user.getbirth(),
                       "cpf": user.getcpf(),
@@ -20,76 +24,50 @@ class UserControl:
                       "password": user.getpwd(),
                       "admin": user.getadmin()
                       })
-        with open("Model/Users.txt", "w") as file:
-            file.write(str(lista))
+        with open(locale, "w") as file:
+            file.write(str(users))
 
-    def get_users(self, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-                lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-        return lista
+    def get_users(self):
+        return users
 
-    def get_by_id(self, id, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-               lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-            for i in range(len(lista)):
-                if lista[i]["id"] == id:
-                    return lista[i]
-                    break
-            else:
-                return []
-    def get_by_email(self, email, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-               lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-            for i in range(len(lista)):
-                if lista[i]["email"] == email:
-                    return lista[i]
-                    break
-            else:
-                return []
+    def get_by_id(self, id):
+        for i in range(len(users)):
+            if users[i]["id"] == id:
+                return users[i]
+                break
+        else:
+            return []
 
-    def get_by_cpf(self, cpf, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-               lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-            for i in range(len(lista)):
-                if lista[i]["cpf"] == cpf:
-                    return lista[i]
-                    break
-            else:
-                return []
+    def get_by_email(self, email):
+        for i in range(len(users)):
+            if users[i]["email"] == email:
+                return users[i]
+                break
+        else:
+            return []
+
+    def get_by_cpf(self, cpf):
+        for i in range(len(users)):
+            if users[i]["cpf"] == cpf:
+                return users[i]
+                break
+        else:
+            return []
 
     def update(self, user, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            lista = literal_eval(file)
-        for i in range(len(lista)):
-            if lista[i]["id"] == user.getid():
-                if not user.getname() == lista[i]["name"]:
-                    lista[i]["name"] = user.getname()
-                if not user.getbirth() == lista[i]["birth"]:
-                    lista[i]["birth"] = user.getbirth()
-                if not user.getcpf() == lista[i]["cpf"]:
-                    lista[i]["cpf"] = user.getcpf()
-                if not user.getemail() == lista[i]["email"]:
-                    lista[i]["email"] = user.getemail()
-                if not user.getpwd() == lista[i]["password"]:
-                    lista[i]["password"] = user.getpwd()
-                if not user.getadmin() == lista[i]["admin"]:
-                    lista[i]["admin"] = user.getadmin()
+        for i in range(len(users)):
+            if users[i]["id"] == user.getid():
+                if not user.getname() == users[i]["name"]:
+                    users[i]["name"] = user.getname()
+                if not user.getbirth() == users[i]["birth"]:
+                    users[i]["birth"] = user.getbirth()
+                if not user.getcpf() == users[i]["cpf"]:
+                    users[i]["cpf"] = user.getcpf()
+                if not user.getemail() == users[i]["email"]:
+                    users[i]["email"] = user.getemail()
+                if not user.getpwd() == users[i]["password"]:
+                    users[i]["password"] = user.getpwd()
+                if not user.getadmin() == users[i]["admin"]:
+                    users[i]["admin"] = user.getadmin()
         with open(locale, "w") as file:
-            file.write(str(lista))
+            file.write(str(users))
