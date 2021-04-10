5596648580194745777cd374639448a6faba267f
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-04-10 19:41:32 -0300

    Update ProductController.py
    Modificado para funcionar com objetos

--- Controller/ProductController.py
+++ Controller/ProductController.py
@@ -11,76 +11,70 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+products = []
+with open("Model/Products.txt", "r") as file:
+    file = file.read()
+    try:
+        lista = literal_eval(file)
+    except:
+        raise Exception("Erro no banco de dados. Não foi possível converter ID.")
+products = lista
+
 class ProductController():
     def add_product(self, product, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            lista = literal_eval(file)
-        if len(lista) == 0:
+        if len(products) == 0:
             id = 1
         else:
-            id = str(int(lista[len(lista) - 1]["id"]) + 1)
-        lista.append({"id": id,
+            id = str(int(products[len(products) - 1]["id"]) + 1)
+        products.append({"id": id,
                       "name": product.getname(),
                       "price": product.getprice(),
                       "category": product.getcategory()
                       })
         with open(locale, "w") as file:
-            file.write(str(lista))
-    def get_products(self, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-                lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-        return lista
-    def get_by_id(self, id, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-                lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter ID.")
-            for i in range(len(lista)):
-                if lista[i]["id"] == id:
-                    return lista[i]
-                    break
-            else:
-                return []
-    def update(self, product, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            lista = literal_eval(file)
-        for i in range(len(lista)):
-            if lista[i]["id"] == product.getid():
-                if not lista[i]["name"] == product.getname():
-                    lista[i]["name"] = product.getname()
-                if not lista[i]["price"] == product.getprice():
-                    lista[i]["price"] = product.getprice()
-                if not lista[i]["category"] == product.getcategory():
-                    lista[i]["category"] = product.getcategory()
+            file.write(str(products))
 
+    def get_products(self):
+        return products
+
+    def get_by_id(self, id):
+        for i in range(len(products)):
+            if products[i]["id"] == id:
+                return products[i]
+                break
+        else:
+            return []
+
+    def update(self, product, locale):
+        for i in range(len(products)):
+            if products[i]["id"] == product.getid():
+                if not products[i]["name"] == product.getname():
+                    products[i]["name"] = product.getname()
+                if not products[i]["price"] == product.getprice():
+                    products[i]["price"] = product.getprice()
+                if not products[i]["category"] == product.getcategory():
+                    products[i]["category"] = product.getcategory()
         with open(locale, "w") as file:
-            file.write(str(lista))
+            file.write(str(products))
+
     def delete(self, id, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            lista = literal_eval(file)
-        for i in range(len(lista)):
-            if lista[i]["id"] == id:
-                lista.pop(i)
+        for i in range(len(products)):
+            if products[i]["number"] == products:
+                products.pop(i)
+        with open(locale, "w") as file:
+            file.write(str(products))
+            file.close()
 
     def buy(self, cart, user, card):
         from View import UserView
-        print("cart = "+str(cart))
         for i in cart:
-            print(i)
-            if int(i["price"]) <= int(card["funds"]):
+            if float(i["price"]) <= float(card["funds"]):
                 from Controller import CardController
-                CardController.cardcontrol().update(card["number"], i["price"], "Model/Cards.txt")
+                price = str(float(card["funds"]) - float(i["price"]))
+                CardController.cardcontrol().update(card["number"], price, "Model/Cards.txt")
                 print(f"{bcolors.OKGREEN}Compra de {i['name']} no valor de R${i['price']} realizada com sucesso!{bcolors.ENDC}")
             else:
                 print(f"{bcolors.FAIL}Falha na Compra: Você não possui saldo suficiente no cartão.{bcolors.ENDC}")
                 UserView.main().showusercategorys(user)
+        UserView.main().clearcart()
         UserView.main().showusercategorys(user)
