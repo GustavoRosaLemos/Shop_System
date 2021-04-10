d8242ee51ce519a7a7669e8e5997a0d2b66f812f
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-04-10 18:30:35 -0300

    Update CardController.py
    Atualizado o card controller para funcionar com objetos, afim de evitar o problema de atualização dos arquivos txt.

--- Controller/CardController.py
+++ Controller/CardController.py
@@ -1,64 +1,69 @@
 from ast import literal_eval
 
+cards = []
+with open("Model/Cards.txt", "r") as file:
+    try:
+        lista = literal_eval(file.read())
+    except:
+        raise Exception("Erro no banco de dados. Não foi possível converter numero.")
+    file.close()
+cards = lista
+
 class cardcontrol:
     def add_card(self, card, locale):
+        cards.append({"number": card.getnumber(),
+                       "cvv": card.getccv(),
+                       "date": card.getdate(),
+                       "funds": card.getfunds()
+                       })
         with open(locale, "r") as file:
             file = file.read()
             lista = literal_eval(file)
-        lista.append({"number": card.get_number(),
+        lista.append({"number": card.getnumber(),
                       "cvv": card.getccv(),
                       "date": card.getdate(),
-                      "founds": card.getfunds()
+                      "funds": card.getfunds()
                       })
         with open(locale, "w") as file:
             file.write(str(lista))
+            file.close()
 
-    def getcards(self, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-                lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter numero.")
-        return lista
+    def getcards(self):
+        return cards
 
-    def get_by_number(self, number, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            try:
-                lista = literal_eval(file)
-            except:
-                raise Exception("Erro no banco de dados. Não foi possível converter numero.")
-            for i in range(len(lista)):
-                if lista[i]["number"] == str(number):
-                    return lista[i]
-                    break
-            else:
-                return []
+    def get_by_number(self, number):
+        for i in range(len(cards)):
+            if cards[i]["number"] == str(number):
+                return cards[i]
+                break
+        else:
+            return []
 
     def update(self, number, funds, locale):
+        for i in range(len(cards)):
+            if cards[i]["number"] == number:
+                cards[i]["funds"] = funds
         with open(locale, "r") as file:
             file = file.read()
             lista = literal_eval(file)
         for i in range(len(lista)):
             if lista[i]["number"] == number:
-                intlista = int(lista[i]["funds"])
-                intfunds = int(funds)
-                intlista -= intfunds
-                lista[i]["funds"] = str(intlista)
-
-
-
-        with open(locale, "w") as file:
-            file.write(str(lista))
-
-    def delete(self, number, locale):
-        with open(locale, "r") as file:
-            file = file.read()
-            lista = literal_eval(file)
-        for i in range(len(lista)):
-            if lista[i]["number"] == number:
-                lista.pop(i)
+                lista[i]["funds"] = funds
         file = open(locale, "w")
         file.write(str(lista))
         file.close()
+
+    def delete(self, number, locale):
+        for i in range(len(cards)):
+            if cards[i]["number"] == number:
+                cards.pop(i)
+        with open(locale, "r") as file:
+            file = file.read()
+            lista = literal_eval(file)
+            file.close()
+        for i in range(len(lista)):
+            if lista[i]["number"] == number:
+                lista.pop(i)
+        with open(locale, "w") as file:
+            file.write(str(lista))
+            file.close()
